f08243345c870beb05e915d90cc00267a8cb0b0f
Grant Gonzalez <84807512+grantgonzalez14@users.noreply.github.com>
2022-12-08 19:51:58 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,19 @@
     }
 
     :root {
-        --primary-color: #14110F;
+        --primary-color: #000000;
         --secondary-color: #7E7F83;
         --tertiary-color: #00205B;
         --quaternary-color: #F3F3F4;
         --linkedin-blue: #0072b1;
+    }
+
+    #backgroundVideo {
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        min-width: 100%;
+        min-height: 100%;
+        z-index: -10;
     }
 </style>
