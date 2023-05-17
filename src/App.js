d43051aff4704a957c63abaf7961b9cb60cce1ea
Grant Gonzalez <grantgonzalez14@gmail.com>
2023-05-17 13:51:31 -0400

    Fixed navbar navigation scoll positioning

--- src/App.js
+++ src/App.js
@@ -21,57 +21,44 @@ function App() {
 		<>
 			<Navbar scrollFunction={scroll}/>
 			<Parallax pages={6} ref={pref}>
+				<ParallaxLayer offset={0} speed={0.5} factor={12}  className='parallax'/>
 				
-				<ParallaxLayer offset={0} speed={0.5} factor={12}  className='parallax'>
-					
-				</ParallaxLayer>
-				
-				<ParallaxLayer offset={0} speed={1}>
-					<Element className='element' name='Home'>
+				<ParallaxLayer offset={0} speed={1} className='layer'>
+					<Element name='Home'>
 						<Home />
 					</Element>
 				</ParallaxLayer>
 				
-				<ParallaxLayer offset={1} speed={1}>
-					<Element className='element' name='About'>
-						<About id='About'/>
+				<ParallaxLayer offset={1} speed={1} className='layer'>
+					<Element name='About'>
+						<About />
 					</Element>
 				</ParallaxLayer>
 				
-				<ParallaxLayer offset={2} speed={1}>
-					<Element className='element' name='Education'>					
+				<ParallaxLayer offset={2} speed={1} className='layer'>
+					<Element name='Education'>					
 						<Education />
 					</Element>
 				</ParallaxLayer>
 				
-				<ParallaxLayer offset={3} speed={1}>
-					<Element className='element' name='Skills'>
+				<ParallaxLayer offset={3} speed={1} className='layer'>
+					<Element name='Skills'>
 						<Skills />
 					</Element>
 				</ParallaxLayer>
 				
-				<ParallaxLayer offset={4} speed={1}>
-					<Element className='element' name='Projects'>
+				<ParallaxLayer offset={4} speed={1} className='layer'>
+					<Element name='Projects'>
 						<Projects />
 					</Element>
 				</ParallaxLayer>
 
-				<ParallaxLayer offset={5} speed={1}>
-					<Element className='element' name='Contact'>
+				<ParallaxLayer offset={5} speed={1} className='layer'>
+					<Element name='Contact'>
 						<Contact />
 					</Element>
 				</ParallaxLayer>
 			</Parallax>
-
-			{/* <div className='app-container'>
-				<Navbar />
-				<Element name='Home' ><Home /></Element>
-				<Element name='About'><About /></Element>
-				<Element name='Education'><Education /></Element>
-				<Element name='Skills'><Skills /></Element>
-				<Element name='Projects'><Projects /></Element>
-				<Element name='Contact'><Contact /></Element>
-			</div> */}
 		</>
 	);
 }
