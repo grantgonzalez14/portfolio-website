54934b25e8acc4707c47cb172c1dca0059771f30
Grant Gonzalez <grantgonzalez14@gmail.com>
2024-04-13 18:00:05 -0400

    Added dynamic sizing and positioning for parallax items. Will resize and re-position on window resize and based on initial dimensions

--- src/App.js
+++ src/App.js
@@ -9,56 +9,223 @@ import Projects from './sections/js/Projects';
 import Contact from './sections/js/Contact';
 import { Element } from 'react-scroll';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import { useRef } from 'react';
-
-// const url = (name) =>
-//   `url(../pulbic/assets/images/${name})`
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
 	Aos.init();
 
+	let windowWidth = window.innerWidth;
+	let windowHeight = window.innerHeight;
+
+	const [isVerticalLayout, setVerticalLayout] = useState(windowHeight > windowWidth);
+	
 	let pref = useRef();
 	let scroll = (to) => {pref.current.scrollTo(to)};
+	let sizeObject = {
+		satellite: windowWidth * 0.16071429,
+		earth: (isVerticalLayout ? '80' : '100'),
+		astronaut: windowWidth * 0.06696429,
+		rocketShip: windowWidth * 0.04464286
+	};
+
+	let positionObject = {
+		satellite: {
+			left: (windowWidth <= 600 ? 0 : 5)
+		},
+		earth: {
+			top: (isVerticalLayout && windowWidth > 600 ? (windowHeight - windowWidth) : (isVerticalLayout && windowWidth <= 600 ? (windowHeight - windowWidth) / 2 : 0) ),
+			bottom: 0,
+			left: (isVerticalLayout && windowWidth > 600 ? '14%' : '3rem')
+		},
+		astronaut: {
+			top: 0,
+			bottom: 0,
+			right: 0,
+			left: 0
+		},
+		rocketShip: {
+			top: 0,
+			bottom: 0,
+			right: 0,
+			left: 0
+		},
+		cloud: [
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			}
+		],
+		opaqueCloud: [
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			},
+			{
+				top: 0,
+				bottom: 0,
+				right: 0,
+				left: 0
+			}
+		]
+	}
+	
+	const [size, setSize] = useState(sizeObject);
+	const [position, setPosition] = useState(positionObject);
+
+	useEffect(() => {
+		const calculateSizes = () => {
+			setVerticalLayout(windowWidth > windowHeight);
+			let newSizes = {
+				satellite: windowWidth * 0.16071429,
+				earth: (isVerticalLayout ? '80' : '100'),
+				astronaut: windowWidth * 0.06696429,
+				rocketShip: windowWidth * 0.04464286
+			};
+			setSize(newSizes);
+		};
+
+		const calculatePositions = () => {
+			let newPositions = {
+				satellite: {
+					left: (windowWidth <= 600 ? 0 : 5)
+				},
+				earth: {
+					top: (isVerticalLayout && windowWidth > 600 ? (windowHeight - windowWidth) : (isVerticalLayout && windowWidth <= 600 ? (windowHeight - windowWidth) / 2 : 0) ),
+					left: (isVerticalLayout && windowWidth > 600 ? '14%' : '3rem')
+				}
+			};
+			setPosition(newPositions);
+		};
+
+		const handleResize = () => {
+			calculatePositions();
+			calculateSizes();
+		};
+		
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, [windowWidth, windowHeight, isVerticalLayout]);
 
 	return (
 		<>
 			<Navbar scrollFunction={scroll}/>
 			<Parallax pages={6} ref={pref}>
 				{/* Background Image Layers */}
-				<ParallaxLayer offset={0} speed={0} factor={12} className='stars' style={{backgroundRepeat: 'repeat', zIndex: 2, top: 0}} />
 				<ParallaxLayer offset={0} speed={0.5} factor={12} className='parallax' style={{ zIndex: 1 }}/>
-				<ParallaxLayer offset={5.5} speed={-0.4} className='earth' style={{zIndex: 3, backgroundSize: 'cover', marginLeft: '3rem'}}/>
-				<ParallaxLayer offset={1.25} speed={-0.3} className='satelliteOne' style={{zIndex: 3, display: 'block', marginLeft: '87%', backgroundSize: '360px 360px'}} />
-				<ParallaxLayer offset={2.5} speed={-0.3} className='satelliteTwo' style={{zIndex: 3, display: 'block', marginLeft: '3%', backgroundSize: '360px 360px'}} />
-				<ParallaxLayer offset={3.75} speed={-0.3} className='satelliteThree' style={{zIndex: 3, display: 'block', marginLeft: '85%', backgroundSize: '360px'}} />
-
-				<ParallaxLayer offset={5.52} speed={0.5} className='astronautCharacter' style={{zIndex: 4, display: 'block', marginLeft: '13%', backgroundSize: '150px'}} />
-				<ParallaxLayer offset={5.5} speed={0.5} className='rocketShip' style={{zIndex: 4, display: 'block', marginLeft: '18%', backgroundSize: '100px'}} />
+				<ParallaxLayer offset={0} speed={0} factor={12} className='stars' style={{backgroundRepeat: 'repeat', zIndex: 2, top: 0}} />
+				<ParallaxLayer offset={5.5} speed={-0.4} className='earth' style={{zIndex: 3, backgroundSize: `${size.earth}%`, marginTop: `${position.earth.top}px`, marginLeft: `${position.earth.left}`}}/>
+				<ParallaxLayer offset={1.25} speed={-0.3} className='satelliteOne' style={{zIndex: 3, display: 'block', marginLeft: '87%', backgroundSize: `${size.satellite}px`}} />
+				<ParallaxLayer offset={2.5} speed={-0.3} className='satelliteTwo' style={{zIndex: 3, display: 'block', marginLeft: `${position.satellite.left}rem`, backgroundSize: `${size.satellite}px`}} />
+				<ParallaxLayer offset={3.75} speed={-0.3} className='satelliteThree' style={{zIndex: 3, display: 'block', marginLeft: '83%', backgroundSize: `${size.satellite}px`}} />
+				<ParallaxLayer offset={5.52} speed={0.5} className='astronautCharacter' style={{zIndex: 4, display: 'block', marginLeft: '13%', backgroundSize: `${size.astronaut}px`}} />
+				<ParallaxLayer offset={5.5} speed={0.5} className='rocketShip' style={{zIndex: 4, display: 'block', marginLeft: '18%', backgroundSize: `${size.rocketShip}px`}} />
 
 				{/* ---------- CLOUDS ---------- */}
 				{/* Bottom Right */}
-				<ParallaxLayer offset={5.5} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '77%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.5} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '77%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.5} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '75%', backgroundSize: '15%'}} />
 				{/* Top Right */}
-				<ParallaxLayer offset={5.1} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '67%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.1} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '67%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.1} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '65%', backgroundSize: '15%'}} />
 				{/* Second to Bottom Right */}
-				<ParallaxLayer offset={5.3} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '55%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.3} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '55%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.3} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '53%', backgroundSize: '15%'}} />
 				{/* Top Middle */}
-				<ParallaxLayer offset={5} speed={-0.2} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '47%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5} speed={-0.2} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '47%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5} speed={-0.2} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '45%', backgroundSize: '15%'}} />
 				{/* Bottom Middle */}
-				<ParallaxLayer offset={5.25} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '37%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.25} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '37%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.25} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '35%', backgroundSize: '15%'}} />
 				{/* Top Left */}
-				<ParallaxLayer offset={5.05} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '27%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.05} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '27%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.05} speed={-0.26} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '25%', backgroundSize: '15%'}} />
 				{/* Second to bottom left */}
-				<ParallaxLayer offset={5.35} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '19%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.35} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '19%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.35} speed={-0.23} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '17%', backgroundSize: '15%'}} />
 				{/* Bottom Left */}
-				<ParallaxLayer offset={5.55} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '7%', opacity: '0.4'}} />
+				<ParallaxLayer offset={5.55} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '7%', opacity: '0.4', backgroundSize: '20%'}} />
 				<ParallaxLayer offset={5.55} speed={-0.4} className='cloud' style={{zIndex: 3, display: 'block', marginLeft: '5%', backgroundSize: '15%'}} />
 				
 				{/* Element Layers */}
@@ -92,7 +259,7 @@ function App() {
 					</Element>
 				</ParallaxLayer>
 
-				<ParallaxLayer offset={5} speed={1} className='layer' style={{ zIndex: 3 }}>
+				<ParallaxLayer offset={5} speed={1} className='layer' style={{ zIndex: 5}}>
 					<Element name='Contact'>
 						<Contact />
 					</Element>
--- src/sections/js/Home.js
+++ src/sections/js/Home.js
@@ -5,33 +5,33 @@ import '../css/Home.scss';
 function Home() {
     const data_aos_animation_time = 2000;
     const [mobile, setMobile] = useState(isMobile);
-    const [responseData, setResponseData] = useState(null);
-    const [isLoading, setIsLoading] = useState(null);
-    const [index, setIndex] = useState(0);
-    const [error, setError] = useState(null);
-    const [loadingTextList] = useState(['This joke is going to be hilarious...', 'Get ready to laugh...', 'Here it comes!']);
-    const [loadingText, setLoadingText] = useState(loadingTextList[index]);
+    // const [responseData, setResponseData] = useState(null);
+    // const [isLoading, setIsLoading] = useState(null);
+    // const [index, setIndex] = useState(0);
+    // const [error, setError] = useState(null);
+    // const [loadingTextList] = useState(['This joke is going to be hilarious...', 'Get ready to laugh...', 'Here it comes!']);
+    // const [loadingText, setLoadingText] = useState(loadingTextList[index]);
 
     
 
-    const tellJoke = () => {
-        // try {
-        //     const response = await fetch('api call here');
-        //     if (!response.ok) {
-        //         throw new Error('Failed to fetch data');
-        //     }
-        //     const data = await response.json();
-        //     setResponseData(data);
-        //     setIsLoading(false);
-        // } catch (error) {
-        //     setError(error);
-        //     setIsLoading(false);
-        // }
-        setIsLoading(true);
-        setTimeout(() => {
-            setResponseData({data: 'received!'});
-        })
-    };
+    // const tellJoke = () => {
+    //     // try {
+    //     //     const response = await fetch('api call here');
+    //     //     if (!response.ok) {
+    //     //         throw new Error('Failed to fetch data');
+    //     //     }
+    //     //     const data = await response.json();
+    //     //     setResponseData(data);
+    //     //     setIsLoading(false);
+    //     // } catch (error) {
+    //     //     setError(error);
+    //     //     setIsLoading(false);
+    //     // }
+    //     setIsLoading(true);
+    //     setTimeout(() => {
+    //         setResponseData({data: 'received!'});
+    //     })
+    // };
     
     useEffect(() => {
         const handleResize = () => setMobile(window.innerWidth <= 925);
@@ -43,20 +43,20 @@ function Home() {
     //     tellJoke();
     // }, []);
 
-    useEffect(() => {
-        if (isLoading) {
-            const updateText = () => {
-                if (responseData !== null) {
-                    clearInterval(intervalID);
-                    return;
-                }
-                setLoadingText(loadingTextList[index]);
-                setIndex(prevIndex => (prevIndex === loadingTextList.length - 1 ? prevIndex : prevIndex + 1));
-            }
-            const intervalID = setInterval(updateText, 3000);
-            return () => clearInterval(intervalID);
-        }
-    }, [isLoading, responseData, index, loadingTextList]);
+    // useEffect(() => {
+    //     if (isLoading) {
+    //         const updateText = () => {
+    //             if (responseData !== null) {
+    //                 clearInterval(intervalID);
+    //                 return;
+    //             }
+    //             setLoadingText(loadingTextList[index]);
+    //             setIndex(prevIndex => (prevIndex === loadingTextList.length - 1 ? prevIndex : prevIndex + 1));
+    //         }
+    //         const intervalID = setInterval(updateText, 3000);
+    //         return () => clearInterval(intervalID);
+    //     }
+    // }, [isLoading, responseData, index, loadingTextList]);
 
     return (
         <>
@@ -91,11 +91,11 @@ function Home() {
                     clients and team members. That's why I'm committed to staying up-to-date with the latest trends and best
                     practices in full-stack development, and I'm always looking for new opportunities to learn and grow.<br/><br/>
                 </h2>
-                {isLoading === true && 
+                {/* {isLoading === true && 
                 <h2>
                     {loadingText}
                 </h2>}
-                <button className='joke-button' onClick={tellJoke}>Click for a joke!</button>
+                <button className='joke-button' onClick={tellJoke}>Click for a joke!</button> */}
             </div>
         </>
     );
